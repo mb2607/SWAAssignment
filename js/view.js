fb9e42da2087fe0a49525f464808d73774a9796d
mb2607 <martin.bertelsen1@gmail.com>
2016-10-11 15:45:47 +0200

    cleanup and seperation of updatefulltable method and addtotable method

--- js/view.js
+++ js/view.js
@@ -3,10 +3,10 @@ function View() {
     
     var table = document.getElementById('fruitsTable');
  
-    
-    this.fillTable = function(controller){
+    var updateFullTable = function(){
+         var fruits = controller.show();
         
-        var fruits = controller.show();
+        $("#fruitsTable tr").remove();
         
         for(var i=0; i < fruits.length; i++){
             var tr = table.appendChild(document.createElement('tr'));
@@ -17,71 +17,56 @@ function View() {
         } 
     }
     
-    
-        buttonAdd.onclick=function(){
-            var textInput = document.getElementById('textInput').value;
-            var oldLength = controller.show().length;
-
-            controller.add(textInput);
+    var addNodeToTable = function(){
+        var textInput = document.getElementById('textInput').value;
+        var oldLength = controller.show().length;
+                
+        controller.add(textInput);
+                
+        var newLength = controller.show().length;
+        alert('old = '+ oldLength + ' new = ' + newLength);
             
-             var newLength = controller.show().length;
-            alert('old = '+ oldLength + ' new = ' + newLength);
-            
-            var fruits = controller.show();
+        var fruits = controller.show();
+        //compare old table length to new table length, if different, add node
         for(var i=oldLength; i < newLength; i++){
             var tr = table.appendChild(document.createElement('tr'));
             var td = tr.appendChild(document.createElement('td'));
             td.appendChild(document.createTextNode(fruits[i]));
             tr.appendChild(td);
             table.appendChild(tr);
-        } 
-        }
+        }     
+    }
+    
+    this.fillTable = function(controller){
+        updateFullTable();
+    }
+    
+    buttonAdd.onclick=function(){
         
-        buttonDelete.onclick=function(){
-            var textInput = document.getElementById('textInput').value;
-            var oldLength = controller.show().length;
-
-            controller.deleteByName(textInput);
-            
-             var newLength = controller.show().length;
-            alert('old = '+ oldLength + ' new = ' + newLength);
-            
-            var fruits = controller.show();
-            
-            $("#fruitsTable tr").remove(); 
-
-        for(var i=0; i < fruits.length; i++){
-            var tr = table.appendChild(document.createElement('tr'));
-            var td = tr.appendChild(document.createElement('td'));
-            td.appendChild(document.createTextNode(fruits[i]));
-            tr.appendChild(td);
-            table.appendChild(tr);
-        } 
-        }
+        addNodeToTable();
+    }
         
-        buttonEdit.onclick=function(){
-            var textInput = document.getElementById('textInput').value;
-            var editInput = document.getElementById('editInput').value;
+    buttonDelete.onclick=function(){
+        var textInput = document.getElementById('textInput').value;
+        var oldLength = controller.show().length;
 
-            controller.edit(textInput, editInput);
-            alert('you are about to change: ' + textInput + ' into: ' + editInput);
-            
-            var fruits = controller.show();
-            
-            $("#fruitsTable tr").remove(); 
+        controller.deleteByName(textInput);
 
-        for(var i=0; i < fruits.length; i++){
-            var tr = table.appendChild(document.createElement('tr'));
-            var td = tr.appendChild(document.createElement('td'));
-            td.appendChild(document.createTextNode(fruits[i]));
-            tr.appendChild(td);
-            table.appendChild(tr);
-        } 
-        }
-    
- 
-    
-       
-    
-    
+        var newLength = controller.show().length;
+        alert('old = '+ oldLength + ' new = ' + newLength);
+
+        updateFullTable(); 
+    }
+        
+    buttonEdit.onclick=function(){
+        var textInput = document.getElementById('textInput').value;
+        var editInput = document.getElementById('editInput').value;
+
+        controller.edit(textInput, editInput);
+        alert('you are about to change ' + textInput + ' into ' + editInput);
+
+        var fruits = controller.show();
+
+        updateFullTable(); 
+    }
 }
