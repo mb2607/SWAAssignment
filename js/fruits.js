d7b58578ed2fafb55161211ffc05838fd3f67923
Skuli Skulason <Skuli Skulason>
2016-11-21 20:30:18 +0100

    Created fruits objects

---
+++ js/fruits.js
@@ -0,0 +1,51 @@
+function Banana(price, origin) {
+    Object.defineProperties(this, {
+        price: {
+            value: price
+            , writable: true
+        }
+        , origin: {
+            value: origin
+            , false
+        }
+    });
+}
+
+function Apple(price, origin) {
+    Object.defineProperties(this, {
+        price: {
+            value: price
+            , writable: true
+        }
+        , origin: {
+            value: origin
+            , false
+        }
+    });
+}
+
+function Orange(price, origin) {
+    Object.defineProperties(this, {
+        price: {
+            value: price
+            , writable: true
+        }
+        , origin: {
+            value: origin
+            , false
+        }
+    });
+}
+
+function PineApple(price, origin) {
+    Object.defineProperties(this, {
+        price: {
+            value: price
+            , writable: true
+        }
+        , origin: {
+            value: origin
+            , false
+        }
+    });
+}
